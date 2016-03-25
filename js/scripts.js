08f3382c09544208c423f094261d51e308884ba4
Nicholas Arrasate <nicholasarrasate@Nicholass-MacBook-Pro.local>
2016-03-25 15:22:47 -0700

    correct indentation and spacing. It will also add a placeholder text to the 'display' portion of the page with the necessary JS to remove it upon successful form submission.

--- js/scripts.js
+++ js/scripts.js
@@ -2,55 +2,57 @@ $(document).ready(function() {
 
   $("form#radios").submit(function( event ) {
 
-    var name = $("#namer").val();
+    var name = document.getElementsByName("name")[0].value;
     var climate = $("input:radio[name=climateRadio]:checked").val();
     var tree = $("input:radio[name=treeRadio]:checked").val();
     var passport = $("input:radio[name=passRadio]:checked").val();
     var city = $("input:radio[name=cityRadio]:checked").val();
     var kids = $("input:radio[name=kidsRadio]:checked").val();
 
-    $(document).trigger("clear-alert-id.example");
-    if (name.length <= 1) {
-      $(document).trigger("set-alert-id-example", [
-        {
-          message: "Please enter a name",
-          priority: "error"
-        }])
-        console.log("here");
-      }
+    $("#alert").empty();
 
-    if (climate === "hot" && tree === "palm" && passport === "no"       && city === "city") {
+    if (name === null || name === ""){
+      $("#alert").text("Please enter a name and resubmit.");
+
+    } else if(climate === "hot" && tree === "palm" && passport === "no"       && city === "city") {
+      $(".default").addClass("hidedefault");
       $(".showme").removeClass("showme");
       $(".hawaii").addClass("showme");
 
     } else if (climate === "hot" && tree === "palm" && passport   === "yes" && city === "country") {
+      $(".default").addClass("hidedefault");
       $(".showme").removeClass("showme");
       $(".fiji").addClass("showme");
 
     } else if (climate === "cold" && tree === "pine" && passport === "no" && city === "country") {
+      $(".default").addClass("hidedefault");
       $(".showme").removeClass("showme");
       $(".alaska").addClass("showme");
 
     } else if (climate === "cold" && passport === "yes" && city === "country" && kids === "no") {
+      $(".default").addClass("hidedefault");
       $(".showme").removeClass("showme");
       $(".iceland").addClass("showme");
 
     } else if (climate === "temperate" && passport === "yes" && city === "city") {
+      $(".default").addClass("hidedefault");
       $(".showme").removeClass("showme");
       $(".rome").addClass("showme");
 
     } else if (climate === "temperate" && passport === "no" && city === "city") {
+      $(".default").addClass("hidedefault");
       $(".showme").removeClass("showme");
       $(".newyork").addClass("showme");
 
     } else {
+      $(".default").addClass("hidedefault");
       $(".showme").removeClass("showme");
       $(".error").addClass("showme");
     }
 
+    $(".name").text(name);
 
-      $(".name").text(name);
-      event.preventDefault();
+    event.preventDefault();
 
     });
 });
